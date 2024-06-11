5d12571ee730e3302287bea365e9d248bc2ec5bc
Aeldro <nodepuydt@gmail.com>
2024-06-11 22:53:05 +0200

    isColumnValid coded but bugs.

--- ValideSudoku/Program.cs
+++ ValideSudoku/Program.cs
@@ -5,9 +5,9 @@
         static void Main(string[] args)
         {
 
-            static bool areCharactersValid(int[,] input)
+            static bool areCharactersValid(string[,] input)
             {
-                char[] validCharaters = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '.' };
+                string[] validCharaters = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "." };
 
                 for (int i = 0; i < input.GetLength(0); i++)
                 {
@@ -29,7 +29,7 @@
                 return true;
             }
 
-            static bool isRowValid(int[,] input)
+            static bool isRowValid(string[,] input)
             {
                 for (int i = 0; i < input.GetLength(0); i++)
                 {
@@ -37,19 +37,38 @@
                     {
                         for (int k = 0; k < input.GetLength(1); k++)
                         {
-                            if (input[i, j] != input[i, k])
+                            if (j != k && input[i, j] != "." && input[i, j] == input[i, k])
                             {
-
+                                return false;
                             }
                         }
                     }
                 }
+                return true;
             }
 
-            static bool isSudokuValid(int[,] input)
+            static bool isColumnValid(string[,] input)
+            {
+                for (int i = 0; i < input.GetLength(1); i++)
+                {
+                    for (int j = 0; j < input.GetLength(0); j++)
+                    {
+                        for (int k = 0; k < input.GetLength(0); k++)
+                        {
+                            if (j != k && input[j, i] != "." && input[j, i] == input[j, k])
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+                return true;
+            }
+
+            static bool isSudokuValid(string[,] input)
             {
 
-                if (areCharactersValid(input))
+                if (areCharactersValid(input) && isRowValid(input))
                 {
                     return true;
                 }
@@ -59,6 +78,10 @@
                 }
 
             }
+
+            string[,] expectedTrue = { { "5", "3", ".", ".", "7", ".", ".", ".", "." }, { "6", ".", ".", "1", "9", "5", ".", ".", "." }, { ".", "9", "8", ".", ".", ".", ".", "6", "." }, { "8", ".", ".", ".", "6", ".", ".", ".", "3" }, { "4", ".", ".", "8", ".", "3", ".", ".", "1" }, { "7", ".", ".", ".", "2", ".", ".", ".", "6" }, { ".", "6", ".", ".", ".", ".", "2", "8", "." }, { ".", ".", ".", "4", "1", "9", ".", ".", "5" }, { ".", ".", ".", ".", "8", ".", ".", "7", "9" } };
+            string[,] expectedFalse = { { "8", "3", ".", ".", "7", ".", ".", ".", "." }, { "6", ".", ".", "1", "9", "5", ".", ".", "." }, { ".", "9", "8", ".", ".", ".", ".", "6", "." }, { "8", ".", ".", ".", "6", ".", ".", ".", "3" }, { "4", ".", ".", "8", ".", "3", ".", ".", "1" }, { "7", ".", ".", ".", "2", ".", ".", ".", "6" }, { ".", "6", ".", ".", ".", ".", "2", "8", "." }, { ".", ".", ".", "4", "1", "9", ".", ".", "5" }, { ".", ".", ".", ".", "8", ".", ".", "7", "9" } };
+            Console.WriteLine(isSudokuValid(expectedFalse));
         }
     }
 }
